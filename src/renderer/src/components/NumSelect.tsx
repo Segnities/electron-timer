b21e183b31c46887777afda1bed0bf8dbc7bb820
unknown <dimab787898@gmail.com>
2024-06-18 18:32:40 +0300

    focus & arrow btns

--- src/renderer/src/components/NumSelect.tsx
+++ src/renderer/src/components/NumSelect.tsx
@@ -19,6 +19,8 @@ function NumSelect(props: Props): JSX.Element {
   const { min, max, name, value, setValue } = props
   const dispatch = useDispatch()
 
+  const [isActive, setIsActive] = useState<boolean>(false)
+
   const [positive1Range, setPositive1Range] = useState<string>(getNextRangeNum(value, min, max, 1))
   const [positive2Range, setPositive2Range] = useState<string>(getNextRangeNum(value, min, max, 2))
 
@@ -49,6 +51,26 @@ function NumSelect(props: Props): JSX.Element {
     }
   }
 
+  const handleFocus = (): void => setIsActive(true)
+  const handleBlur = (): void => setIsActive(false)
+  const handleMouseEnter = (): void => setIsActive(true)
+  const handleMouseLeave = (): void => setIsActive(false)
+
+  const handleKeyDown = (e: KeyboardEvent): void => {
+    if (isActive) {
+      if (e.key === 'ArrowUp') {
+        e.preventDefault()
+        playSoundDir()
+        setValue((prevValue) => parseInt(getPrevRangeNum(prevValue, min, max, 1)))
+      } else if (e.key === 'ArrowDown') {
+        e.preventDefault()
+
+        playSoundDir()
+        setValue((prevValue) => parseInt(getNextRangeNum(prevValue, min, max, 1)))
+      }
+    }
+  }
+
   useEffect(() => {
     setNegative1Range(getPrevRangeNum(value, min, max, 1))
     setNegative2Range(getPrevRangeNum(value, min, max, 2))
@@ -56,11 +78,27 @@ function NumSelect(props: Props): JSX.Element {
     setPositive2Range(getNextRangeNum(value, min, max, 2))
   }, [value])
 
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+
+    return ()=> {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [isActive])
+
   return (
-    <div className={styles['select-list']} onWheel={handleMouseWheel}>
+    <div
+      className={styles['select-list']}
+      onWheel={handleMouseWheel}
+      tabIndex={0}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <span className={styles['range-2']}>{negative2Range}</span>
       <span className={styles['range-1']}>{negative1Range}</span>
-      <span className={styles['selected']}>{toRangeNum(value)}</span>
+      <span className={`${styles['selected']}`}>{toRangeNum(value)}</span>
       <span className={styles['range-1']}>{positive1Range}</span>
       <span className={styles['range-2']}>{positive2Range}</span>
     </div>
@@ -68,3 +106,4 @@ function NumSelect(props: Props): JSX.Element {
 }
 
 export default NumSelect
+
